a627f90a804f2a0cd58123d6d8226686de3c5f26
jefynho1213 <jefynho1213@hotmail.com>
2016-10-11 16:26:51 -0300

    Add files via upload

---
+++ dash/components/navbar.php
@@ -0,0 +1,13 @@
+	<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		  	<div class="navbar-header">
+      			<a class="navbar-brand" href="#">Dashboard</a>
+    		</div>
+		    <ul class="nav navbar-nav">
+		     <li><a ui-sref="home"> <button ng-disabled="false" type="button" class="btn btn btn-primary-outlin">MIO</button></a></li>
+		   <li><a ui-sref="ccn">  <button ng-disabled="false" type="button" class="btn btn btn-primary-outlin">CCN</button></a></li>
+		    <li><a ><button ng-disabled="true" type="button" class="btn btn btn-primary-outlin">ECE</button></a></li>
+		    <li><a ><button ng-disabled="true" type="button" class="btn btn btn-primary-outlin">APN Create</button></a></li>
+		    </ul>
+		  </div>
+	</nav>
